feb3c4b7c9afc57e6fe5b7921490cbd781d33b99
Jigar Varmora <102030526+varmoraj@users.noreply.github.com>
2025-12-24 23:42:04 +0530

    Add masterData array with purchaser records

---
+++ data.js
@@ -0,0 +1,21 @@
+const masterData = [
+  {
+    sr: 1,
+    purchaserTIN: "03022043153",
+    purchaserCST: "03022043153",
+    name: "BRAR'S YAMAHA",
+    series: "PB/AA/C",
+    serial: "9312089",
+    date: "2015-07-01"
+  },
+  {
+    sr: 2,
+    purchaserTIN: "03712077160",
+    purchaserCST: "03712077160",
+    name: "KUDRAT ENTERPRISES",
+    series: "PB/AA/C",
+    serial: "9312093",
+    date: "2015-07-01"
+  }
+  // ... continue all records
+];
